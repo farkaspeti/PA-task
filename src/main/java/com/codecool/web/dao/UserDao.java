67d3cd62c2b19bc8229d0bacef58ce98abac69ae
farkaspeti <farkas.peti97@gmail.com>
2019-07-01 18:58:14 +0200

    UserDao upgraded

--- src/main/java/com/codecool/web/dao/UserDao.java
+++ src/main/java/com/codecool/web/dao/UserDao.java
@@ -3,8 +3,13 @@ package com.codecool.web.dao;
 import com.codecool.web.model.User;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface UserDao {
 
     User findByEmail(String email) throws SQLException;
+    
+    void add(String email, String password, String firstName, String lastName) throws SQLException;
+    
+    List<User> findAll() throws SQLException;
 }
