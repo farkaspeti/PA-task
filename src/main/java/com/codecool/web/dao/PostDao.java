6a19d52c275dd771aacfd7c532aa40ad00fd0f2b
farkaspeti <farkas.peti97@gmail.com>
2019-07-02 14:33:05 +0200

    PostDao implemented

---
+++ src/main/java/com/codecool/web/dao/PostDao.java
@@ -0,0 +1,21 @@
+package com.codecool.web.dao;
+
+import com.codecool.web.model.Post;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface PostDao {
+
+    List<Post> findAll() throws SQLException;
+    
+    Post findById(int postId) throws SQLException;
+    
+    Post add(int userId, String content, String postDate) throws SQLException;
+    
+    void update(int postId, String content) throws SQLException;
+    
+    void delete(int postId) throws SQLException;
+    
+    List<Post> findAllByUserId(int userId) throws SQLException;
+}
