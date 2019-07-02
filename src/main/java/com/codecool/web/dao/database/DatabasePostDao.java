559d3d896e940b19b63315a4cfe7348cbb6b5567
farkaspeti <farkas.peti97@gmail.com>
2019-07-02 14:41:19 +0200

    fetchPost created

--- src/main/java/com/codecool/web/dao/database/DatabasePostDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabasePostDao.java
@@ -4,7 +4,10 @@ import com.codecool.web.dao.PostDao;
 import com.codecool.web.model.Post;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatabasePostDao extends AbstractDao implements PostDao {
@@ -13,7 +16,16 @@ public class DatabasePostDao extends AbstractDao implements PostDao {
     
     @Override
     public List<Post> findAll() throws SQLException {
-        return null;
+        List<Post> postList = new ArrayList<>();
+        String sql = "SELECT * FROM posts order by post_id";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                postList.add(fetchPost(resultSet));
+            }
+        
+        }
+        return postList;
     }
     
     @Override
@@ -39,5 +51,13 @@ public class DatabasePostDao extends AbstractDao implements PostDao {
     @Override
     public List<Post> findAllByUserId(int userId) throws SQLException {
         return null;
+    }
+    
+    private Post fetchPost(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("post_id");
+        int userId = resultSet.getInt("user_id");
+        String content = resultSet.getString("content");
+        String postDate = resultSet.getString("post_date");
+        return new Post(id, userId, content, postDate);
     }
 }
