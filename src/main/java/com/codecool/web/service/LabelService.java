b699da51f7903f481f7e2808a0cd5cfe042b68bb
farkaspeti <farkas.peti97@gmail.com>
2019-07-03 16:52:36 +0200

    LabelService created

--- src/main/java/com/codecool/web/service/LabelService.java
+++ src/main/java/com/codecool/web/service/LabelService.java
@@ -1,4 +1,4 @@
-package com.codecool.web.dao;
+package com.codecool.web.service;
 
 import com.codecool.web.model.Label;
 import com.codecool.web.service.exception.ServiceException;
@@ -6,10 +6,9 @@ import com.codecool.web.service.exception.ServiceException;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface LabelDao {
+public interface LabelService {
     
     List<Label> findAll() throws SQLException, ServiceException;
     
-    Label add(String labelContent) throws SQLException, ServiceException;
-    
+    Label addLabel(String labelContent) throws SQLException, ServiceException;
 }
