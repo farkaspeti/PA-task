2a217ffb572469e160e0c7fdf6adf561e5cd28e2
farkaspeti <farkas.peti97@gmail.com>
2019-07-03 16:41:55 +0200

    PostService created

---
+++ src/main/java/com/codecool/web/service/PostService.java
@@ -0,0 +1,27 @@
+package com.codecool.web.service;
+
+import com.codecool.web.model.Post;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface PostService {
+    
+    Post addPost(int userId, String content) throws SQLException, ServiceException;
+    
+    List<Post> findAllByUserId(int userId) throws SQLException, ServiceException;
+    
+    void updatePost(int postId, String content) throws SQLException,ServiceException;
+    
+    void deletePost(int postId) throws SQLException, ServiceException;
+    
+    void addLabelToPost(int postId, int labelId) throws SQLException, ServiceException;
+    
+    Post findById(int postId) throws SQLException,ServiceException;
+    
+    List<Post> findAll() throws SQLException,ServiceException;
+    
+    List<Integer> findPostIdByLabelId(int labelId) throws SQLException,ServiceException;
+    
+}
