5baa12a19fee60e38719a17324ff2888daf3d0e1
farkaspeti <farkas.peti97@gmail.com>
2019-07-19 10:08:12 +0200

    Refactored Post model

--- src/main/java/com/codecool/web/model/Post.java
+++ src/main/java/com/codecool/web/model/Post.java
@@ -5,16 +5,24 @@ import java.sql.Date;
 public final class Post extends AbstractModel {
     
     private final int userId;
+    private final String firstName;
+    private final String lastName;
     private final String content;
     private final Date postDate;
     
     
-    public Post(int id, int userId, String content,Date postDate) {
+    public Post(int id, int userId,String firstName,String lastName, String content,Date postDate) {
         super(id);
         this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.content = content;
         this.postDate = postDate;
     }
+    
+    public String getFirstName() { return firstName; }
+    
+    public String getLastName() { return lastName; }
     
     public int getUserId() {
         return userId;
